5faf43743cd4fe7a4b9d8c7ebf93abcb487eeb23
Marco Zank <52705419+mzank@users.noreply.github.com>
2026-04-24 00:11:46 +0200

    docs: improve Sphinx config for stable autodoc build and suppress pandas type warnings

--- docs/conf.py
+++ docs/conf.py
@@ -1,20 +1,29 @@
-project = 'market-analysis'
-copyright = '2026, Marco Zank'
-author = 'Marco Zank'
-release = '0.1.0'
+project = "market-analysis"
+copyright = "2026, Marco Zank"
+author = "Marco Zank"
+release = "0.1.0"
 
 extensions = [
-    'autodoc2',
-    'myst_parser',
-    'sphinx_rtd_theme',
+    "autodoc2",
+    "myst_parser",
+    "sphinx_rtd_theme",
 ]
 
-exclude_patterns = ['_build']
+exclude_patterns = ["_build"]
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
 autodoc2_packages = [
     "../src/market_analysis",
 ]
 autodoc2_render_plugin = "myst"
 autodoc2_output_dir = "apidocs"
+
+autodoc_typehints = "description"
+autodoc_typehints_format = "short"
+
+nitpick_ignore = [
+    ("py:class", "pandas.DataFrame"),
+    ("py:class", "pandas.Series"),
+    ("py:class", "pandas._typing.TimestampConvertibleTypes"),
+]
